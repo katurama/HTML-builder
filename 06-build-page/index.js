00a42905675d41df11ae7277ea8e0be499f665c1
Katya Emelyanchik <katuramakaturama@gmail.com>
2022-05-24 14:45:47 +0300

    feat: add copy Assets to 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
 const fsN = require ('fs');
+const { allowedNodeEnvironmentFlags } = require('process');
 fs.mkdir(path.join(__dirname,'project-dist'), { recursive: true });
 const htmlBuilder =  async () => {
   await fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), '');
@@ -31,3 +32,48 @@ const sccBundle =  () => {
   });
 };
 sccBundle();
+
+const copyAssets = () => {
+  fsN.readdir(path.join(__dirname, 'assets'),  (error, files) => {
+    if (error) throw error;
+    files.forEach(file => {
+      fsN.stat(path.join(__dirname, 'assets', file), (error, stats)=>{
+        if(error) throw error;
+        if(stats.isDirectory()){
+          fs.mkdir(path.join(__dirname,'/project-dist', 'assets', file), { recursive: true });
+          fsN.readdir(path.join(__dirname, 'assets', file),  (error, copyFiles) => {
+            copyFiles.forEach(copyFile => {
+              fs.copyFile(path.join(__dirname, 'assets', file, copyFile), path.join(__dirname, '/project-dist', 'assets', file, copyFile) );
+            });
+            
+          });
+        } else if(stats.isFile()){
+          fs.copyFile(path.join(__dirname, 'assets', file), path.join(__dirname, '/project-dist', 'assets', file) );
+        }
+      });
+    });
+
+    
+    fsN.readdir(path.join(__dirname, '/project-dist', 'assets'),  (error, filesC) => {
+      if (error) throw error;
+      filesC.forEach(elem => {
+        if(!files.includes(elem)){
+          fsN.readdir(path.join(__dirname, '/project-dist', 'assets', elem),  (error, elemFile) => {
+            if (error) throw error;  
+            elemFile.forEach(eF => {
+              fs.unlink(path.join(__dirname, 'project-dist', 'assets', elem, eF));
+            });
+            fsN.rmdir(path.join(__dirname,'/project-dist', 'assets',elem), err => {
+              if(err) throw err;
+            });
+          });
+          
+          
+        }
+      });
+    });        
+  });
+  
+  fs.mkdir(path.join(__dirname,'/project-dist', 'assets'), { recursive: true });
+};
+copyAssets();
